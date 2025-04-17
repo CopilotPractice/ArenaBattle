8ee74080a4c5e91bc1079c6867ebdeeb809566c7
KimChanHoe <kch467@gmail.com>
2025-04-17 12:21:13 +0900

    250417

--- Source/ArenaBattleDemo/Character/ABCharacterBase.cpp
+++ Source/ArenaBattleDemo/Character/ABCharacterBase.cpp
@@ -5,12 +5,22 @@
 #include "ABCharacterControlData.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "ABComboActionData.h"
+#include "Physics/ABCollision.h"
+#include "Components/CapsuleComponent.h"
+#include "Engine/DamageEvents.h"
 
 // Sets default values
 AABCharacterBase::AABCharacterBase()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//컴포넌트 설정
+	GetCapsuleComponent()->SetCollisionProfileName(CPROFILE_ABCAPSULE);
+
+	//메시의 콜리전은 NoCollision 설정 (주로 랙돌에 사용됨)
+	GetMesh()->SetCollisionProfileName(TEXT("NoCollision"));
+
 
 	static ConstructorHelpers::FObjectFinder<UABCharacterControlData> ShoulderDataRef(
 		TEXT("/Game/ArenaBattle/CharacterControl/ABC_Shoulder.ABC_Shoulder"));
@@ -31,6 +41,18 @@ AABCharacterBase::AABCharacterBase()
 			QuarterDataRef.Object
 		);
 	}
+
+	static ConstructorHelpers::FObjectFinder<UAnimMontage> ComboActionMontageRef (TEXT("/Game/ArenaBattle/Animation/AM_ComboAttack.AM_ComboAttack"));
+	if (ComboActionMontageRef.Object)
+	{
+		ComboActionMontage = ComboActionMontageRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UABComboActionData> ComboActionDataRef(TEXT("/Game/ArenaBattle/ComboAction/ABA_ComboAction.ABA_ComboAction"));
+	if (ComboActionDataRef.Object)
+	{
+		ComboActionData = ComboActionDataRef.Object;
+	}
 }
 
 void AABCharacterBase::SetCharacterControlData(const UABCharacterControlData* InCharacterControlData)
@@ -42,6 +64,97 @@ void AABCharacterBase::SetCharacterControlData(const UABCharacterControlData* In
 	GetCharacterMovement()->bOrientRotationToMovement = InCharacterControlData->bOrientRotationToMovement;
 	GetCharacterMovement()->bUseControllerDesiredRotation = InCharacterControlData->bUseControllerDesiredRotation;
 	GetCharacterMovement()->RotationRate = InCharacterControlData->RotationRate;
+}
+
+void AABCharacterBase::AttackHitCheck()
+{
+	//공격 판정 진행
+	//UE_LOG(LogTemp, Log, TEXT("AttackHitCheck !!"));
+
+	// 충돌 시작 지점 계산
+	//캐릭터 몸통에서 약간 앞으로(캡슐의 반지름 만큼) 설정
+	FVector Start = GetActorLocation() + GetActorForwardVector() * GetCapsuleComponent()->GetScaledCapsuleRadius();
+
+	//공격 거리
+	const float AttackRange = 50.0f;
+	FVector End = Start + GetActorForwardVector() * AttackRange;
+
+	// SCENE_QUERY_STAT : 언리얼에서 지원하는 분석 툴에 태그를 추가
+	// 두번째 인자 : 복잡한 형태의 충돌체를 감지할 지 여부
+	// 세번째 인자 : 무시할 액터 목록
+	FCollisionQueryParams Params(SCENE_QUERY_STAT(Attack), false, this);
+
+	// 트레이스에 사용할 구체의 반지름
+	const float AttackRadius = 50.0f;
+
+	// 트레이스를 활용하여 충돌 검사
+	FHitResult OutHitResult;
+	bool HitDetected = GetWorld()->SweepSingleByChannel(
+		OutHitResult,
+		Start,
+		End,
+		FQuat::Identity, //쿼터니언(사원수)에서 회전을 안 한 상태
+		CCHANNEL_ABACTION,
+		FCollisionShape::MakeSphere(AttackRadius),
+		Params
+	);
+
+	//충돌 감지된 경우의 처리
+	if (HitDetected)
+	{
+		//데미지 양
+		const float AttackDamage = 30.0f;
+
+		//데미지 이벤트
+		FDamageEvent DamageEvent;
+
+		// 데미지 전달 
+		//. 구조체 접근, -> 클래스 접근
+		OutHitResult.GetActor()->TakeDamage(
+			AttackDamage,
+			DamageEvent,
+			GetController(),
+			this
+		);
+	}
+
+	//충돌 디버그(시각적으로 확인할 수 있도록)
+#if ENABLE_DRAW_DEBUG
+
+	//캡슐 중심 위치
+	FVector CapsuleOrigin = Start + (End - Start) * 0.5f;
+
+	//캡슐 높이 절반 값
+	float CapsuleHalfHeight = AttackRange * 0.5f;
+
+	//표시할 색상 (안 맞았으면 빨강, 맞았으면 초록)
+	FColor DrawColor = HitDetected ? FColor::Green : FColor::Red;
+
+	//캡슐 그리기
+	DrawDebugCapsule(GetWorld(),
+		CapsuleOrigin,
+		CapsuleHalfHeight,
+		AttackRadius,
+		FRotationMatrix::MakeFromZ(GetActorForwardVector()).ToQuat(), // 오일러 회전 행렬에서 쿼터니언으로 변환
+		DrawColor,
+		false,
+		5.0f //선을 얼마나 오래 그릴지
+	);
+#endif
+}
+
+float AABCharacterBase::TakeDamage(
+	float DamageAmount, 
+	FDamageEvent const& DamageEvent, 
+	AController* EventInstigator, 
+	AActor* DamageCauser)
+{
+	Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	// @Task : 맞으면 바로 죽도록 처리
+
+
+	return DamageAmount;
 }
 
 void AABCharacterBase::ProcessComboCommand()
---
+++ Source/ArenaBattleDemo/Character/ABCharacterNonePlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ABCharacterBase.h"
+#include "ABCharacterNonePlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLEDEMO_API AABCharacterNonePlayer : public AABCharacterBase
+{
+	GENERATED_BODY()
+	
+};
